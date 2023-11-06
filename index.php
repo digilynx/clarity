56fd4ad07273f4bbdd1d72097dd69fe581f8c0ce
digilynx <72888214+digilynx@users.noreply.github.com>
2023-11-06 19:08:50 +0500

    Update index.php
    change fond size in index text

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "<div style='font-size:30px.color:green,padding:100px;'>3Scale API Management</div>";
+echo "<div style='font-size:30px;color:green,padding:100px;font-weight:bold;'>3Scale API Management</div>";
 ?>
