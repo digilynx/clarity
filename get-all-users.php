e95e9d1d23128e98357164ec986c860953ae2788
digilynx <72888214+digilynx@users.noreply.github.com>
2023-11-06 19:38:52 +0500

    Update and rename home.php to get-all-users.php
    add getallusers file

---
+++ get-all-users.php
@@ -0,0 +1,30 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$result = $connection->query("SELECT * FROM users order by id asc");
+
+//Initialize array variable
+$dbdata = [];
+
+//Fetch into associative array
+while ($row = $result->fetch_assoc()) {
+  $dbdata[] = $row;
+}
+
+//Print array in JSON format
+//echo json_encode($dbdata);
+$data=array('userDetails'=>$dbdata);
+echo '<pre>'.print_r($data,true).'</pre>';
+
+$connection->close();
+?>
