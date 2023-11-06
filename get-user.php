dc00620c66fd150173c9a909b43ba2f5a9a624ef
digilynx <72888214+digilynx@users.noreply.github.com>
2023-11-06 19:59:08 +0500

    Update get-user.php
    hhh

--- get-user.php
+++ get-user.php
@@ -22,10 +22,9 @@ while ($row = $result->fetch_assoc()) {
   $dbdata[] = $row;
 }
 
-//Print array in JSON format
-//echo json_encode($dbdata);
 $data=array('userDetails'=>$dbdata);
-echo '<pre>'.print_r($data,true).'</pre>';
+echo json_encode($data);
+//echo '<pre>'.print_r($data,true).'</pre>';
 
 $connection->close();
 ?>
